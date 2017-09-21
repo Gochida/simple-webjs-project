364074ea599bf2b83eb34f05410d41ec3b664945
Gochida <yobi.gochida@gmail.com>
2017-09-21 11:58:24 -0700

    updated to fit Standard style
    Using 'Standard' styling

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,32 @@
-var gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    uglify = require('gulp-uglify'),
-    browserify = require('gulp-browserify'),
-    browserSync = require('browser-sync');
+const gulp = require('gulp')
+const gutil = require('gulp-util')
+const uglify = require('gulp-uglify')
+const browserify = require('gulp-browserify')
+const browserSync = require('browser-sync')
 
-gulp.task('js', function(){
-    return gulp.src('src/*.js')
-            .pipe(browserify())
-            .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
-            .pipe(uglify())
-            .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
-            .pipe(gulp.dest('dist/js'));
+gulp.task('js', function () {
+  return gulp.src('src/*.js')
+          .pipe(browserify())
+          .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()) })
+          .pipe(uglify())
+          .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()) })
+          .pipe(gulp.dest('dist/js'));
 })
 
-gulp.task('reload', function(done) {
-    browserSync.reload();
-    done();
-});
+gulp.task('reload', function (done) {
+  browserSync.reload()
+  done()
+})
 
+gulp.task('default', ['js'], function () {
+  // serve files
+  browserSync.init({
+    server: {
+      baseDir: './site'
+    }
+  })
 
-gulp.task('default', ['js'], function(){
-    //serve files
-    browserSync.init({
-        server: {
-            baseDir:"./dist"
-        }
-    });
-
-    //watch js files for changes
-    gulp.watch(["src/*.js"], ['js'])
-    gulp.watch(["dist/**/*.*"], ['reload']);
+    // watch js files for changes
+  gulp.watch([`src/*.js`], ['js'])
+  gulp.watch([`site/**/*.*`], ['reload'])
 })
